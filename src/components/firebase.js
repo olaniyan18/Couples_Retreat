b270df191f168ba46c58067929a74f9e9f60786c
Your Name <your.email@example.com>
2025-10-18 13:42:58 +0100

    fixed

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCEfIX8WN2yGtRaFQKux3NORKOHAiy5CWo",
   authDomain: "couples-retreat-f48b0.firebaseapp.com",
   projectId: "couples-retreat-f48b0",
-  storageBucket: "couples-retreat-f48b0.firebasestorage.app",
+  storageBucket: "couples-retreat-f48b0.appspot.com",
   messagingSenderId: "886830200949",
   appId: "1:886830200949:web:b6f3553412bc49f723e03c",
   measurementId: "G-SKBR4J5XWP",
